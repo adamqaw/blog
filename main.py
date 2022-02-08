b4fe0886bdc7b4ca7c2b62b3ef604770c2f381a0
Adam Al-Qawasmeh <82409325+adamqaw@users.noreply.github.com>
2022-02-08 12:42:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from news import News
+from weather import Weather
 
 app = Flask(__name__)
 
@@ -12,7 +13,18 @@ def home():
 @app.route('/news')
 def news():
     rass = News()
-    return render_template('news.html', news=rass)
+    titles = rass.article_titles
+    descriptions = rass.article_descriptions
+    content = rass.article_content
+    authors = rass.article_authors
+    return render_template('news.html', news=rass, titles=titles, descriptions=descriptions, content=content, authors=authors)
+
+
+@app.route('/weather')
+def weather():
+    w = Weather()
+    report = w.report
+    return render_template('weather.html', weather=report)
 
 
 if __name__ == '__main__':

095338efba94ed615f210d12b5f1b9496d334181
Adam Al-Qawasmeh <82409325+adamqaw@users.noreply.github.com>
2022-06-24 16:31:45 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,6 +1,13 @@
 import requests
+import datetime
+import os
 
-API_KEY = ''
+API_KEY = os.eviron.get('polygon_api_key')
 
-polygon_endpoint = ''
 
+class Stock:
+    def __init__(self, stocksTicker, date):
+        self.stocksTicker = stocksTicker
+        self.date = date
+        self.polygon_endpoint = f'https://api.polygon.io/v1/open-close/{self.stocksTicker}/{self.date}?apiKey={API_KEY}'
+        self.response = requests.get(self.polygon_endpoint).json()
